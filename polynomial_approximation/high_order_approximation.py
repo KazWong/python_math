717dfcacc38ee4e186536127fae5856751634989
Kaz Wong <kaz_wong@sae.com.hk>
2019-03-08 12:36:13 +0800

    true high order

--- polynomial_approximation/high_order_approximation.py
+++ polynomial_approximation/high_order_approximation.py
@@ -3,11 +3,16 @@ import numpy as np
 import random
 import matplotlib.pyplot as plt
 
-sample = 5
-sigma = 1000
+sample = 100
+order = 20
+sigma = 1000000
+
+if (not sample > order):
+  order = sample - 1
+  
 p_x = []
 p_y = []
-A = np.ndarray(shape=(0, 2))
+A = np.ndarray(shape=(0, order))
 B = np.ndarray(shape=(0, 1))
 
 o_m = random.uniform(-20, 20)
@@ -16,26 +21,34 @@ o_c = random.uniform(-20, 20)
 for i in xrange(sample):
   p_x.append( random.uniform(-1000, 1000) )
   p_y.append( random.gauss(o_m * p_x[-1] + o_c, random.uniform(0, sigma)) )
-  A = np.append( A, np.array([1., p_x[-1], p_x[-1]*p_x[-1]]) )
+  
+  order_array = [1.]
+  for j in xrange(order):
+    order_array.append( math.pow(p_x[-1], j+1) )
+  A = np.append( A, np.array(order_array) )
   B = np.append( B, np.array(p_y[-1]) )
   
 o_x = np.linspace(-1100,1100,100)
 o_y = o_m*o_x+o_c
 
 
-A = A.reshape([-1, 3])
+A = A.reshape([-1, order+1])
 B = B.reshape([-1, 1])
 A_t = A.transpose();
 A = np.linalg.inv(A_t.dot(A)).dot(A_t)
-
 a = A.dot(B)
-a0 = a[0]
-a1 = a[1]
-a2 = a[2]
 
-x = np.linspace(-1100,1100,100)
-y = a2*x*x+a1*x+a0
 
+x = np.linspace(-800,800,100)
+y = []
+for i in xrange(len(x)):
+  tmp = 0.
+  for j in xrange(order + 1):
+    tmp = a[j, 0] * math.pow(x[i], j) + tmp
+  y.append(tmp)
+
+for i in xrange(order + 1):
+  print "a" + str(i) + " =", a[i, 0]
 
 plt.subplot(111)
 plt.xlabel('x')
