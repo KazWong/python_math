3617233c4d8197e27141f7876d889682542dae17
Kaz Wong <KazvonWong@gmail.com>
2019-03-14 14:33:53 +0800

    fixed unit bug in dry air

---
+++ kalman_filter/simple_filter.py
@@ -0,0 +1,55 @@
+import math
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+from ..plant.dry_air import DryAir
+from ..plant.linear_gaussian import LinearGaussian
+
+vol = 1.
+init_temp = 25.
+plant = DryAir( 10, vol, init_temp, LinearGaussian(0., 0., 0.), LinearGaussian(0.6, 0., 0.) )
+
+A = [1.]
+X = [20.] # State
+P = [1.] # Process covariance matrix
+B = [1./(1005.*plant.Density(init_temp)*vol)]
+u = [0.] # control variable matrix
+w = [0.] # Process noise
+Q = [0.] # Process noise covariance matrix
+R = [1.] # measurement error covariance matrix
+Y = [] # Observation
+Z = [0.] # Measurement noise
+K = [1.] # Kalman gain
+
+total_measurement = []
+true_y = [25.]
+var = 0.
+
+#without error covariance
+for i in xrange(300):
+  u.append(1.)
+  t, y = plant.Online(u[-1])
+  true_y.append( plant._T )
+  
+  #process convariance
+  total_measurement.append(y)
+  var = (sum(total_measurement)/len(total_measurement) - y) + var
+  
+  #Prediced state
+  Xp = A[-1]*X[-1] + B[-1]*u[-1] + w[-1]
+  P.append( pow(var/len(total_measurement), 2) + Q[-1] )
+  #Measurement state
+  K.append( P[-1] / (P[-1] + R[-1]) )
+  Y.append( y + Z[-1] )
+  X.append( Xp + K[-1]*(Y[-1] - X[-1]) )
+  #Current state
+  P.append( K[-1]*P[-1] )
+
+plt.subplot(111)
+plt.xlabel('t')
+plt.ylabel('y')
+plt.plot(plant.t, plant.y, 'r--', linewidth=0.5)
+plt.plot(plant.t, X)
+plt.plot(plant.t, K)
+plt.plot(plant.t, true_y)
+plt.show()
--- plant/dry_air.py
+++ plant/dry_air.py
@@ -3,29 +3,38 @@ import random
 from disturbance import Disturbance
 
 class DryAir:
-  def __init__(self, _sample_rate, _volume, _temperature, _disturbance = None):
+  def __init__(self, _sample_rate, _volume, _temperature, _disturbance = None, _measure_noise = None):
     self.sample_rate = float(_sample_rate)
     self._volume = float(_volume)
     self.y = []
     self.t = []
     self._init_T = float(_temperature)
     self._T = self._init_T
-    self._Q = 1.01325/( 287.058 * (273.16 + self._T) ) * self._volume * 1005. * (273.16 + self._T)
+    self._Q = self.Density(self._init_T) * self._volume * 1005. * (273.16 + self._T)
     self._Q_new = self._Q
     if (isinstance(_disturbance, Disturbance)):
       self.disturbance = _disturbance
     else:
       self.disturbance = Disturbance()
+    
+    if (isinstance(_measure_noise, Disturbance)):
+      self.measure_noise = _measure_noise
+    else:
+      self.measure_noise = Disturbance()
       
     self.Reset()
     
+  def Density(self, T):
+    return 101325./( 287.058 * (273.16 + float(self._T)) )
+    
   def Model(self, t):
-    density = 1013.25/( 287.058 * (273.16 + self._T) )
     energy_exchange = self._Q_new - self._Q + self.disturbance.Online(t)
-    self._T = self._T + energy_exchange/(density * self._volume * 1005.)
+    self._T = self._T + energy_exchange/(self.Density(self._T) * self._volume * 1005.)
     self._Q = self._Q_new
     
-    return self._T
+    T = self._T + self.measure_noise.Online(t)
+    
+    return T
   
   def Reset(self):
     self.t = [0.]
@@ -46,7 +55,7 @@ class DryAir:
 
   def Online(self, _Q):
     self._Q_new = ( float(_Q) / self.sample_rate ) + self._Q_new # per second change
-    self.t.append(self.t[-1] + 1./self.sample_rate)
+    self.t.append( self.t[-1] + 1./self.sample_rate )
     self.y.append( self.Model(self.t[-1]) )
     
     return self.t[-1], self.y[-1]
