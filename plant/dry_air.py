a1f49974bf7ab5a3b656e74f4fb6b211d4cdb191
Kaz Wong <KazvonWong@gmail.com>
2019-03-15 16:53:26 +0800

    adding input delay in dry air

--- plant/dry_air.py
+++ plant/dry_air.py
@@ -3,15 +3,16 @@ import random
 from disturbance import Disturbance
 
 class DryAir:
-  def __init__(self, _sample_rate, _volume, _temperature, _disturbance = None, _measure_noise = None):
+  def __init__(self, _sample_rate, _input_delay, _volume, _temperature, _disturbance = None, _measure_noise = None):
     self.sample_rate = float(_sample_rate)
     self._volume = float(_volume)
     self.y = []
     self.t = []
+    self.input_delay = _input_delay
     self._init_T = float(_temperature)
     self._T = self._init_T
     self._Q = self.Density(self._init_T) * self._volume * 1005. * (273.16 + self._T)
-    self._Q_new = self._Q
+    self._Q_new = np.full( int(self.sample_rate * self.input_delay), self._Q )
     if (isinstance(_disturbance, Disturbance)):
       self.disturbance = _disturbance
     else:
@@ -28,9 +29,10 @@ class DryAir:
     return 101325./( 287.058 * (273.16 + float(self._T)) )
     
   def Model(self, t):
-    energy_exchange = self._Q_new - self._Q + self.disturbance.Online(t)
+    energy_exchange = self._Q_new[0] - self._Q + self.disturbance.Online(t)
     self._T = self._T + energy_exchange/(self.Density(self._T) * self._volume * 1005.)
-    self._Q = self._Q_new
+    self._Q = self._Q_new[0]
+    np.delete(self._Q_new, [0])
     
     T = self._T + self.measure_noise.Online(t)
     
@@ -42,10 +44,10 @@ class DryAir:
     self._T = self._init_T
   
   def Offline(self, end_t):
-    self.y = []
     self.t = []
+    self.y = []
     
-    sample = float(end_t) * self.sample_rate;
+    sample = float(end_t) * self.sample_rate
     self.t = np.linspace(0., end_t, sample, endpoint=True)
     
     for i in xrange( len(self.t) ):
@@ -54,8 +56,9 @@ class DryAir:
     return self.t, self.y
 
   def Online(self, _Q):
-    self._Q_new = float(_Q) + self._Q_new # per second change
-    self.t.append( self.t[-1] + 1./self.sample_rate )
-    self.y.append( self.Model(self.t[-1]) )
+    self._Q_new = np.append( self._Q_new, float(_Q) + self._Q_new )
+    self.t = np.append( self.t, self.t[-1] + 1./self.sample_rate )
+    self.y = np.append( self.y, self.Model(self.y[-1]) )
+
     
     return self.t[-1], self.y[-1]
