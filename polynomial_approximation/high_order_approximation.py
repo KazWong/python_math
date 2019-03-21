b00c9336a3e113808926d9b62a2ad8b392398290
Kaz Wong <KazvonWong@gmail.com>
2019-03-21 14:18:37 +0800

    high order modified

--- polynomial_approximation/high_order_approximation.py
+++ polynomial_approximation/high_order_approximation.py
@@ -5,12 +5,12 @@ def HighOrderApprox(x, y, order):
   if (not l > order):
     order = l - 1
   
-  A = np.ones([l, 1])
+  A = order_array = np.ones([l, 1])
   B = y.reshape([-1, 1])
-  order_array = xx = x.reshape([-1, 1])
+  xx = x.reshape([-1, 1])
   for i in range(order):
-    A = np.concatenate((A, order_array), axis=1)
     order_array = order_array*xx
+    A = np.concatenate((A, order_array), axis=1)
   
   A_t = A.transpose();
   a = np.linalg.inv(A_t.dot(A)).dot(A_t).dot(B)
