09f4500ac58067c8dd465975fa96969e0d09883f
Kaz@GL504GW <whwong@lscm.hk>
2020-04-14 17:58:17 +0800

    adding pulse simulator

--- dynamic_systems/speed-out.py
+++ dynamic_systems/speed-out.py
@@ -1,31 +1,33 @@
 import math
 import numpy as np
-from . import Dynsys
+#from . import Dynsys
+import matplotlib.pyplot as plt
 
-class SpeedOut(Dynsys):
-  def __init__(self, clock, s, v0, di = None, do = None):
-    super(SpeedOut, self).__init__(clock, di, do)
-    self._ps = float(s0)
-    self._v0 = float(v0)
-    self.Reset()
-    
-  def Reset(self):
-    super(SpeedOut, self).Reset()
-    self._ps = self._s0
-    self._last_v = self._v0
-    self._last_t = 0.
-    
-  def Model(self, t):
-    s = self._dv * (t - self._last_t)
-    
-    if (s >= self.ps):
-      z = t
-    else:
-      z = t
-    
-    #z = z + self.do.Online(t)
-    return z
-  
-  def Online(self, _v):
-    self._dv = self._v - self._last_v
-    return super(SpeedOut, self).Online()
+sample = 10000
+t = np.linspace(0, 2, 10000)
+v = 4*t#-t**2 + 4*t + 1
+
+plt.figure()
+plt.plot(t, v)
+
+
+so = np.pi*0.1565/150.
+dt = 1./sample
+p = [0.0]
+s = 0.0
+
+for i in range(1, sample):
+	s = np.abs(v[i] - v[i-1])*dt + s
+	print(s)
+	#if (s >= so):
+	#	p = np.append(p, [dt*i])
+	#	s = 0.0
+
+l = np.ones(len(p))
+
+
+plt.figure()
+plt.stem(p, l, '-')
+
+
+#plt.show()
