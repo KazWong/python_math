f09510ddde6377f91a2c0b308940788c32e22026
Kaz Wong <KazvonWong@gmail.com>
2020-04-16 11:44:13 +0800

    snapshot

--- dynamic_systems/speed-out.py
+++ dynamic_systems/speed-out.py
@@ -3,31 +3,29 @@ import numpy as np
 #from . import Dynsys
 import matplotlib.pyplot as plt
 
-sample = 10000
-t = np.linspace(0, 2, 10000)
-v = 4*t#-t**2 + 4*t + 1
+sample = 100000
+t = np.linspace(0.0, 4, sample+1)
+v = -t**2 + 4*t + 1
 
 plt.figure()
 plt.plot(t, v)
 
-
-so = np.pi*0.1565/150.
-dt = 1./sample
-p = [0.0]
+dt = t[1] - t[0]
 s = 0.0
+so = 0.5#0.1565*np.pi/150.
+p = []
 
-for i in range(1, sample):
-	s = np.abs(v[i] - v[i-1])*dt + s
-	print(s)
-	#if (s >= so):
-	#	p = np.append(p, [dt*i])
-	#	s = 0.0
+for i in range(0, len(t)):
+  s = v[i]*dt+s
+  if (s >= so):
+    p = np.append(p, [dt*i])
+    s = 0.0
 
 l = np.ones(len(p))
 
-
+#print(s)
 plt.figure()
 plt.stem(p, l, '-')
 
 
-#plt.show()
+plt.show()
