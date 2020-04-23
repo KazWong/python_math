39665e7d542c1a2ce65847540a0b2c5a61d57c43
Kaz@GL504GW <whwong@lscm.hk>
2020-04-23 18:01:25 +0800

    tested combine, fix time, fix position

--- test_bench/tmp_speedout_combine.py
+++ test_bench/tmp_speedout_combine.py
@@ -4,10 +4,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 sample = 100000
-t = np.linspace(0, 10, int(10*sample + 1))
+t = np.linspace(0, 10, int(10*sample)+1)
 a = [0.0, 2.220446049250313e-16, -1.1102230246251565e-16, 0.05000000000000003, -0.007500000000000002, 0.0003000000000000001]
-v = a[0] + a[1]*t + a[2]*t**2 + a[3]*t**3 + a[4]*t**4 + a[5]*t**5
-acc = a[1] + 2*a[2]*t + 3*a[3]*t**2 + 4*a[4]*t**3 + 5*a[5]*t**4
+v = 4.09716*(t-t+1)#a[0] + a[1]*t + a[2]*t**2 + a[3]*t**3 + a[4]*t**4 + a[5]*t**5
+acc = 0*t#a[1] + 2*a[2]*t + 3*a[3]*t**2 + 4*a[4]*t**3 + 5*a[5]*t**4
 
 
 so = np.pi*0.1565/150.
@@ -26,25 +26,39 @@ for i in range(0, len(t)):
 		s = (ds + s) - so
 		ds = 0.0
 	s = s + ds
-	
-#fix time
+
+#Combine
+start_i = 0
 start_t = 0.0
-ct = []
-t_re = []
+j = 0
+tick_count = []
+pulse_count = []
 v_re = []
 a_re = []
-j = 0
+t_re = []
 
-max_count = 100
-update_period = 0.05
-avg_size = 20
+fc = 1000000.
+update_period = 0.01
+avg_size = 5
+
+K = so*fc
+clock = np.linspace(0, 10, int(10*fc)+1)
 for i in range(0, len(p)):
 	if ((p[i] - start_t) >= update_period):
-		ct = np.append(ct, [i-j])
+		tc = 0
+		while (clock[j] < p[i]):
+			j += 1
+			tc += 1
+		if (clock[j] > p[i]):
+			j -= 1
+			tc -= 1
+		tick_count = np.append(tick_count, [tc])
+		pulse_count = np.append(pulse_count, [i-start_i])
+		v_re = np.append(v_re, [ K*pulse_count[-1] / tick_count[-1] ])
 		t_re = np.append(t_re, [p[i]])
-		v_re = np.append(v_re, [6.5554566704907 * (float(ct[-1]) / max_count)])
+		
+		start_i = i
 		start_t = p[i]
-		j = i
 		
 		#accel
 		k = 2
@@ -57,22 +71,21 @@ for i in range(0, len(p)):
 		else:
 			a_re = np.append(a_re, [0.0])
 
-#l = np.ones(len(p))
-#plt.figure()
-#plt.stem(p, l, '-', use_line_collection=True)
-#plt.title("Fix time, Update period = " + str(update_period) + "s, accel "+ str(avg_size) +" value avg") 
+plt.figure()
+plt.stem(t_re, tick_count, '-', use_line_collection=True)
+plt.title("Combine, tick_count = " + str(fc/1.e6) + "MHz")
 
 plt.figure()
-plt.stem(t_re, ct, '-', use_line_collection=True)
-plt.title("Fix time, Update period = " + str(update_period) + "s, accel "+ str(avg_size) +" value avg") 
+plt.stem(t_re, pulse_count, '-', use_line_collection=True)
+plt.title("Combine, pulse_count = " + str(update_period) +"s") 
 
 plt.figure()
-#plt.stem(t_re, v_re, '-', use_line_collection=True)
+#plt.stem(fix_time_t, v_re, '-', use_line_collection=True)
 line_v, = plt.plot(t, v, label='v')
 line_v_re, = plt.plot(t_re, v_re, label='v_re')
 line_a_re, = plt.plot(t_re, a_re, label='a_re')
 line_a, = plt.plot(t, acc, label='a')
 plt.legend(handles=[line_v, line_v_re, line_a_re, line_a])
-plt.title("Fix time, Update period = " + str(update_period) + "s, accel "+ str(avg_size) +" value avg") 
+plt.title("Combine, sampling clock = " + str(fc/1.e6) + "MHz, Update period = "+ str(update_period) +"s, accel "+ str(avg_size) +" value avg") 
 
 plt.show()
--- test_bench/tmp_speedout_fixp.py
+++ test_bench/tmp_speedout_fixp.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 sample = 100000
-t = np.linspace(0, 10, sample+1)
+t = np.linspace(0, 10, int(10*sample + 1))
 a = [0.0, 2.220446049250313e-16, -1.1102230246251565e-16, 0.05000000000000003, -0.007500000000000002, 0.0003000000000000001]
 v = a[0] + a[1]*t + a[2]*t**2 + a[3]*t**3 + a[4]*t**4 + a[5]*t**5
 acc = a[1] + 2*a[2]*t + 3*a[3]*t**2 + 4*a[4]*t**3 + 5*a[5]*t**4
@@ -14,12 +14,6 @@ so = np.pi*0.1565/150.
 p = []
 s = 0.0
 dt = t[1]
-fix_pos = [0]
-fix_pos_t = [0.0]
-v_re = [0.0]
-a_re = [0.0]
-t_a = [0.0]
-count = 0
 
 for i in range(0, len(t)):
 	ds = v[i]*dt
@@ -27,46 +21,70 @@ for i in range(0, len(t)):
 		p = np.append(p, [t[i]])
 		s = 0.0
 		ds = 0.0
-		count += 1
 	elif ((s + ds) > so):
 		p = np.append(p, [t[i-1] + (so - s)/ds * dt])
 		s = (ds + s) - so
 		ds = 0.0
-		count += 1
 	s = s + ds
-#fix time
-Hz = 2000000.#1000000.
-clock = np.linspace(0, 10, int(10*Hz + 1))
+	
+#fix position
+fix_pos = []
+t_re = []
+v_re = []
+a_re = []
+t_a = []
+t_a = []
+ct = []
+count = 0
 j = 0
 count = 0
+k = -2
+
+Hz = 100000.
+interval = 0.1
+
+clock = np.linspace(0, 10, int(10*Hz + 1))
 for i in range(0, len(p)):
 	while (clock[j] < p[i]):
 		j += 1
 		count += 1
+	if (clock[j] > p[i]):
+			j -= 1
+			count -= 1
+	ct = np.append(ct, [count])
 	v_re = np.append(v_re, [ so / (count*(1./Hz)) ])
-	fix_pos_t = np.append(fix_pos_t, [clock[j]])
-	k = 2
-	while ((fix_pos_t[-1] - fix_pos_t[-k]) < 0.1):
-		k += 1
-	a_re = np.append(a_re, [(v_re[-1] - v_re[-k]) / (fix_pos_t[-1] - fix_pos_t[-k])])
+	t_re = np.append(t_re, [clock[j]])
 	count = 0
+	
+	#accel
+	k = -2
+	while ( (len(v_re)+k-1) > 0 and (t_re[-1] - t_re[k-1]) < interval ):
+		k -= 1
+	if ((len(v_re)+k) > 0):
+		a_re = np.append(a_re, [(v_re[-1] - v_re[k]) / (t_re[-1] - t_re[k])])
+		t_a = np.append(t_a, [t_re[-1]])
+	else:
+		a_re = np.append(a_re, [0.0])
+		t_a = np.append(t_a, [t_re[-1]])
 
-l = np.ones(len(p))
-
+#l = np.ones(len(p))
 #plt.figure()
 #plt.stem(p, l, '-', use_line_collection=True)
 
 #plt.figure()
-#plt.stem(fix_pos_t, fix_pos_t, '-', use_line_collection=True)
+#plt.stem(t_re, t_re, '-', use_line_collection=True)
+
+#plt.figure()
+#x = np.linspace(0, len(ct)-1, len(ct))
+#plt.stem(x, ct, use_line_collection=True)
 
 plt.figure()
-
-#plt.stem(fix_pos_t, v_re, '-', use_line_collection=True)
+#plt.stem(t_re, v_re, '-', use_line_collection=True)
 line_v, = plt.plot(t, v, label='v')
-line_v_re, = plt.plot(fix_pos_t, v_re, label='v_re')
-line_a_re, = plt.plot(fix_pos_t, a_re, label='a_re')
+line_v_re, = plt.plot(t_re, v_re, label='v_re')
+line_a_re, = plt.plot(t_a, a_re, label='a_re')
 line_a, = plt.plot(t, acc, label='a')
 plt.legend(handles=[line_v, line_v_re, line_a_re, line_a])
-plt.title("Speed-out Simulation Fix time, sampling clock=2MHz") 
+plt.title("Fix position, sampling clock = " + str(Hz/10.e6) + "MHz, accel interval is " + str(interval) + "s") 
 
 plt.show()
